1a8728f9d7d13515d479d7859132972790fcf995
Jeff S <Slater.Jeff7@gmail.com>
2018-08-02 15:08:39 -0600

    fixed order item issue

--- routes/items.js
+++ routes/items.js
@@ -25,28 +25,6 @@ router.post('/:id', (req,res,next) => {
         })
     })
 })
-//
-// router.get('/', (req,res,next) => {
-//   knex('items')
-//   .then(rows => {
-//     res.json(rows)
-//   })
-//   .catch(err => {
-//     next(err)
-//   })
-// })
-
-// write a route for getting all of the items linked to one owner, respond with the parameter id and make sure the id is converted to a string before sending
-// router.get('/:id', (req,res,next) => {
-//   knex('items')
-//   .where('truck_id',req.params.id)
-//   .then((rows) => {
-//     res.json(rows)
-//   })
-//   .catch((err) => {
-//     next(err)
-//   })
-// })
 
 // write a route for deleting one of the items, respond with the parameter id
 router.delete('/:id/truck/:truckId', (req,res,next) => {
@@ -54,26 +32,16 @@ router.delete('/:id/truck/:truckId', (req,res,next) => {
   .where('item_id', req.params.id)
   .del()
   .then(() => {
-    return knex ('items')
-           .where('id', req.params.id)
-           .del()
-           .then(() => {
-             return knex ('items')
-                    .returning('*')
-                    .then((data) => {
-                      res.status(200).json()
-                    })
-           })
-  })
     return knex('items')
     .where('id', req.params.id)
     .del()
-      .then(() => {
-        return knex('items')
-               .where('truck_id', req.params.truckId)
-               .then(data => {
-                 res.status(200).json(data)
-               })
+    .then(() => {
+      return knex('items')
+      .where('truck_id', req.params.truckId)
+      .then(data => {
+        res.status(200).json(data)
       })
+    })
+  })
 })
 module.exports = router
